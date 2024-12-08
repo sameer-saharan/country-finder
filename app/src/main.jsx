2d8590f5e06e7a6085c870dfae1ad5978b0ddd9e
Sameer Saharan <albaadigoru@gmail.com>
2024-12-08 17:58:08 +0530

    minor changes in App and main

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -3,11 +3,13 @@ import './index.css'
 import App from './App.jsx'
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import {Home, CountryCard} from './components'
+import CountryInfo from './components/CountryInfo.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
       <Route path='' element={<Home />} />
+      <Route path='country/:countryName' element={<CountryInfo />}/>
     </Route>
   )
 );
