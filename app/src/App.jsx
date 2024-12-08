35622ec88724063f60252942e8ffd4853a0ef6fb
Sameer Saharan <albaadigoru@gmail.com>
2024-12-08 16:23:31 +0530

    App layout

---
+++ app/src/App.jsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom"
+import { Home } from "./components"
+
+function App() {
+
+  return (
+    <div className="w-full max-w-screen-sm h-screen mx-auto px-10 responsive">
+      <h1 className="text-6xl font-bold text-center pb-10">Country Finder</h1>
+      <Outlet />
+    </div>
+  )
+}
+
+export default App
