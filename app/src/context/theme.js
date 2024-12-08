edacbe85c6642e0eecfc4c89520a87b04a1488cf
Sameer Saharan <albaadigoru@gmail.com>
2024-12-08 19:32:09 +0530

    updated context

--- app/src/context/theme.js
+++ app/src/context/theme.js
@@ -3,7 +3,9 @@ import { createContext, useContext } from "react";
 const ThemeContext = createContext(
     {
         themeMode: 'light',
-        toggleThemeMode: () => {} 
+        toggleThemeMode: () => {},
+        handleSort: () => {},
+        handleFilter: () => {},
     }
 )
 
