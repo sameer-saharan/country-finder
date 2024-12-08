f046dc347689d315541a3a227cf53a1eebb6d819
Sameer Saharan <albaadigoru@gmail.com>
2024-12-08 16:47:30 +0530

    imported Header in App.jsx

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router-dom"
-import { Home } from "./components"
+import Header from "./components/Header"
 
 function App() {
 
   return (
     <div className="w-full max-w-screen-sm h-screen mx-auto px-10 responsive">
       <h1 className="text-6xl font-bold text-center pb-10">Country Finder</h1>
+      <Header />
       <Outlet />
     </div>
   )
