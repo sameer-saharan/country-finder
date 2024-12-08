3f67dcfcf3fcc672e35ab68453b7024d08ea1b45
Sameer Saharan <albaadigoru@gmail.com>
2024-12-08 17:57:31 +0530

    imporved CountryCard

--- app/src/components/CountryCard.jsx
+++ app/src/components/CountryCard.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
 function CountryCard({country}) {
+    const navigate = useNavigate();
+    const countryName = country.name.common.toLowerCase();
+
+    const handleMoreDetails = () => {
+        navigate(`/country/${countryName}`)
+    };
+
   return (
     <div className='flex flex-col gap-5 p-5 bg-[#d8d7d7] rounded-xl'>
         <img src={country.flags.png} className='w-28'/>
@@ -8,6 +16,12 @@ function CountryCard({country}) {
         <p><span className='font-bold'>Population:</span> {country.population}</p>
         <p><span className='font-bold'>Region:</span> {country.region}</p>
         <p><span className='font-bold'>Capital:</span> {country.capital}</p>
+        <button 
+        className='bg-red-400 rounded-xl py-1 px-2 font-semibold hover:bg-red-500'
+        onClick={handleMoreDetails}
+        >
+            More Details
+        </button>
     </div>
   )
 }
