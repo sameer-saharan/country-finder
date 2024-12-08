4a296b272efbadca547cb227d9259a0bd7fd9d79
Sameer Saharan <albaadigoru@gmail.com>
2024-12-08 16:22:23 +0530

    Home page component

---
+++ app/src/components/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import getAllCountry from '../hooks/getAllCountry'
+import CountryCard from './CountryCard';
+
+function Home() {
+    const countries = getAllCountry();
+  return (
+    <div className='w-full flex flex-wrap gap-10'>
+        {
+            countries.map((country) => (
+                <div key={country.fifa}>
+                    <CountryCard country={country}/>
+                </div>
+            ))
+        }
+    </div>
+  )
+}
+
+export default Home;
